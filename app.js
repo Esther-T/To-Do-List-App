31401211298e5a5c3250ed7a73430c65c1a302ba
Esther-T <tan.esther8@gmail.com>
2021-03-09 21:46:09 -0800

    Enable add to custom lists

--- app.js
+++ app.js
@@ -59,11 +59,23 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
   const itemName = req.body.newItem;
+  const listName = req.body.list;
   const item = new Item({
     name: itemName
   });
-  item.save(); // mongo shortcut for a single item
-  res.redirect("/");
+
+  if (listName === "Today")
+  {
+    item.save(); // mongo shortcut for a single item
+    res.redirect("/");
+  }
+  else {
+    List.findOne({name: listName}, function(err, foundList){
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
+  }
 });
 
 app.post("/delete", function(req, res){
